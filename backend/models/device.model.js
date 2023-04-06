73e195ba23b6ab335c577f179025a5b6866b2edf
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2023-04-06 15:31:46 +0700

    update devide schema

---
+++ backend/models/device.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// Define the parent schema
+const deviceSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    // other device properties
+  });
+  
+  // Define the child schemas
+  const fanSchema = new mongoose.Schema({
+    speed: {
+      type: Number,
+      required: true
+    },
+    // other fan properties
+  });
+  
+  const bulbSchema = new mongoose.Schema({
+    brightness: {
+      type: Number,
+      required: true
+    },
+    // other bulb properties
+  });
+  
+  // Set up the inheritance relationship between the schemas
+  const Device = mongoose.model('Device', deviceSchema);
+  const Fan = Device.discriminator('Fan', fanSchema);
+  const Bulb = Device.discriminator('Bulb', bulbSchema);
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const bcrypt = require('bcrypt');
 
