19cf17309386ae366b77a05e711b825c288470f1
Nguyen <justfornguyen2002@gmail.com>
2023-04-16 11:43:25 +0700

    update DoorSensorRoute.js

--- backend/routes/doorSensorRoute.js
+++ backend/routes/doorSensorRoute.js
@@ -2,13 +2,6 @@ const doorController = require("../controllers/doorController");
 
 const doorRouter = require("express").Router();
 
-// doorRouter.post("/createDoorSensor", doorController.createDoorSensor);
-// doorRouter.get("/getAllDoorSensor", doorController.getAllDoorSensor);
-// doorRouter.get("/getDoorSensorById/:id", doorController.getDoorSensorById);
-// doorRouter.put("/updateDoorSensorStatus/:id/:updateStatus", doorController.updateDoorSensorStatus);
-// doorRouter.delete("/deleteAllDoorSensor", doorController.deleteAllDoorSensor);
-// doorRouter.delete("/deleteDoorSensorById/:id", doorController.deleteDoorSensorById);
-
 doorRouter.post('/create', doorController.create);
 doorRouter.get('/get', doorController.get);
 doorRouter.put('/updateStatus', doorController.updateStatus);
