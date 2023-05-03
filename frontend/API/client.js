41265151b908bb710556d07d71fa4b5f75251ac6
nguyen <justfornguyen2002@gmail.com>
2023-05-04 04:41:08 +0700

    config (don't care about this)

--- frontend/API/client.js
+++ frontend/API/client.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export default axios.create({baseURL: 'http://192.168.1.6:3000'})
+// //When at Dormitory (Nguyen):
+// export default axios.create({baseURL: 'http://192.168.0.6:3000'})
