3caec504110a8486910f15720df0f67047beb734
nguyen <justfornguyen2002@gmail.com>
2023-05-04 17:21:32 +0700

    getAPI for device

--- backend/controllers/deviceController.js
+++ backend/controllers/deviceController.js
@@ -1,16 +1,14 @@
 const DeviceConstructor = require('../models/device.model').deviceModel;
+// const DeviceLog = require('../models/deviceLog.model');
 
 exports.DeviceController = {
     patch: async (req, res) => {
         const deviceId = req.params.deviceId;
-        console.log(req.params.deviceId);
         const state = req.params.state;
         const options = { new: true };
 
         try {
-            console.log('before update device');
-            await DeviceConstructor.findByIdAndUpdate(deviceId, state, options);
-            console.log('update device');
+            await DeviceConstructor.findOneAndUpdate(deviceId, state, options);
             res.status(200).json(state);
         } catch (error) {
             console.error(error);
@@ -23,8 +21,27 @@ exports.DeviceController = {
         const value = req.params.value;
         const options = { new: true };
         try {
-            await DeviceConstructor.findByIdAndUpdate(deviceId, value, options);
+            await DeviceConstructor.findOneAndUpdate(deviceId, value, options);
             res.status(200).json(updates);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({success: false, message: error.message});
+        }
+    },
+
+    getDataByDeviceId: async (req, res) => {
+        try {
+            console.log('GetDeviceHistory!');
+            const deviceInfo = await DeviceConstructor.findOne({_id: req.params.deviceId}).populate({
+                path: 'haveLogs',
+                select: '_id state data'
+            });
+
+            const devicePairs = deviceInfo.haveLogs.map(device => ({
+                _id: device._id, state: device.state, data: device.data
+            }));
+            console.log(devicePairs);
+            res.status(200).json({success: true, data: devicePairs});
         } catch (error) {
             console.error(error);
             res.status(500).json({success: false, message: error.message});
--- backend/routes/deviceRoute.js
+++ backend/routes/deviceRoute.js
@@ -3,9 +3,11 @@ const { DeviceController } = require('../controllers/deviceController');
 const express = require('express');
 const router = express.Router();
 
-const { isAuthenticated } = require('../middlewares/authentication');
+// const { isAuthenticated } = require('../middlewares/authentication');
 
-router.patch('/updateState/:deviceId/:state', isAuthenticated, DeviceController.patch);
-router.patch('/updateValue/:deviceId/:value', isAuthenticated, DeviceController.patchValue);
+router.patch('/updateState/:deviceId/:state', DeviceController.patch);
+router.patch('/updateValue/:deviceId/:value', DeviceController.patchValue);
+
+router.get('/getData/:deviceId', DeviceController.getDataByDeviceId);
 
 module.exports = router;
