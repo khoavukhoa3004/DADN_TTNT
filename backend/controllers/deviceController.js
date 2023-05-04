5b57353d2def4c74f37cf29bfc4f3fab5724a082
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2023-05-04 18:19:59 +0700

    chỉnh lại patch value

--- backend/controllers/deviceController.js
+++ backend/controllers/deviceController.js
@@ -3,16 +3,16 @@ const DeviceConstructor = require('../models/device.model').deviceModel;
 exports.DeviceController = {
     patch: async (req, res) => {
 
-        console.log('deviceController path: ',req.params.deviceId, req.params.state);
+        // console.log('deviceController path: ',req.params.deviceId, req.params.state);
         const deviceId = req.params.deviceId;
         
         const state = req.params.state;
         const options = { new: true };
 
         try {
-            console.log('before update device');
+            console.log('before update device state');
             await DeviceConstructor.findOneAndUpdate({_id: deviceId}, {state}, options);
-            console.log('update device');
+            console.log('update device state');
             res.status(200).json({success: true, message: state});
         } catch (error) {
             console.error(error);
@@ -25,7 +25,8 @@ exports.DeviceController = {
         const value = req.params.value;
         const options = { new: true };
         try {
-            await DeviceConstructor.findByIdAndUpdate(deviceId, value, options);
+            await DeviceConstructor.findOneAndUpdate(deviceId, { value }, options);
+            console.log('update device value');
             res.status(200).json(updates);
         } catch (error) {
             console.error(error);
